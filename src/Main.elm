e392d6c819b3fd813035b0dba2fabd6b1e6b52e4
Francesco Bedussi <francesco.bedussi@ynap.com>
2018-03-20 16:34:53 +0100

    animation

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,11 @@
 module Main exposing (..)
 
 import Css exposing (..)
-import Html.Styled exposing (Html, button, styled, div, h1, input, text)
+import Html.Styled exposing (Html, button, div, h1, input, label, span, styled, text)
 import Html.Styled.Attributes exposing (src, type_, value)
-import Html.Styled.Events exposing (onInput, onClick)
+import Html.Styled.Events exposing (onClick, onInput)
 import Random exposing (int)
+import Time
 
 
 ---- MODEL ----
@@ -18,12 +19,21 @@ type alias Participant =
 
 type alias Model =
     { participants : List Participant
+    , isPlaying : Bool
+    , turns : Int
+    , maxTurns : Int
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model <| buildParticipantList 20, Cmd.none )
+    ( Model
+        (buildParticipantList 20)
+        False
+        0
+        20
+    , Cmd.none
+    )
 
 
 buildParticipantList : Int -> List Participant
@@ -39,6 +49,7 @@ buildParticipantList numberOfParticipant =
 
 type Msg
     = ChangeParticipantsNumber Int
+    | Select
     | Play
     | MarkSelected Int
 
@@ -47,12 +58,23 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ChangeParticipantsNumber participantsNumber ->
-            ( Model <| buildParticipantList participantsNumber, Cmd.none )
+            ( { model | participants = buildParticipantList participantsNumber }, Cmd.none )
 
         Play ->
-            ( {model | participants = resetSelected model.participants}, Random.generate MarkSelected <| Random.int 0 <| List.length model.participants )
+            ( { model | isPlaying = True }, Cmd.none )
+
+        Select ->
+            ( { model
+                | participants = resetSelected model.participants
+              }
+            , Random.generate MarkSelected <| Random.int 0 <| List.length model.participants
+            )
 
         MarkSelected selectedIndex ->
+            let
+                isPlaying =
+                    model.turns < model.maxTurns
+            in
             ( { model
                 | participants =
                     List.indexedMap
@@ -63,6 +85,12 @@ update msg model =
                                 participant
                         )
                         model.participants
+                , isPlaying = isPlaying
+                , turns =
+                    if isPlaying then
+                        model.turns + 1
+                    else
+                        0
               }
             , Cmd.none
             )
@@ -71,46 +99,119 @@ update msg model =
 resetSelected : List Participant -> List Participant
 resetSelected participants =
     participants
-        |> List.map (\participant -> {participant | selected = False})
+        |> List.map (\participant -> { participant | selected = False })
+
+
 
 ---- VIEW ----
 
 
+theme =
+    { colorBackground = hex "000000"
+    , color = hex "00caff"
+    , fontSize = Css.rem 1.5
+    , standardPadding = padding (Css.rem 1)
+    , borderWidth = Css.px 2
+    }
+
+
 view : Model -> Html Msg
 view model =
-    div []
-        ([ h1
+    styled div
+        [ backgroundColor theme.colorBackground
+        , color theme.color
+        , textAlign center
+        , minHeight (Css.vh 100)
+        , width (pct 100)
+        , theme.standardPadding
+        , boxSizing borderBox
+        ]
+        []
+        ([ styled h1
+            [ fontWeight bold
+            , textTransform uppercase
+            , margin3 (Css.rem 0) (Css.rem 1) (Css.rem 2)
+            , theme.standardPadding
+            ]
             []
             [ text "Bologna JS lottery" ]
          ]
-        ++ [ styled div
-                [ displayFlex ]
-                []
-                (model.participants |> List.map (\participant -> renderParticipant participant))
-            ]
-        ++ [ input
-                [ value <| toString <| List.length model.participants
-                , type_ "number"
-                , onInput (\val -> String.toInt val |> Result.withDefault 20 |> ChangeParticipantsNumber)
-                ]
-                []
-            , button
-                [ onClick Play ]
-                [ text "play" ]
-            ]
+            ++ [ styled div
+                    [ width (pct 100)
+                    , marginBottom (Css.rem 3)
+                    ]
+                    []
+                    (model.participants |> List.map (\participant -> renderParticipant participant))
+               ]
+            ++ [ styled label
+                    [marginBottom (Css.rem 1)
+                    , display inlineBlock
+                    , textTransform uppercase
+                    , border3 theme.borderWidth solid theme.color
+                    , theme.standardPadding
+                    ]
+                    []
+                    [ div
+                        []
+                        [ text "participants " ]
+                    , inpt
+                        [ value <| toString <| List.length model.participants
+                        , type_ "number"
+                        , onInput (\val -> String.toInt val |> Result.withDefault 20 |> ChangeParticipantsNumber)
+                        ]
+                        []
+                    ]
+               , div
+                    []
+                    [ btn
+                        [ onClick Play ]
+                        [ text "play" ]
+                    ]
+               ]
         )
 
 
+inpt : List (Html.Styled.Attribute msg) -> List (Html msg) -> Html msg
+inpt = 
+    styled input
+        [padding zero
+        , backgroundColor theme.colorBackground
+        , color theme.color
+        , border zero
+        , textAlign center
+        ]
+
+btn : List (Html.Styled.Attribute msg) -> List (Html msg) -> Html msg
+btn =
+    styled button
+        [ border3 theme.borderWidth solid theme.color
+        , theme.standardPadding
+        , minWidth (Css.rem 2)
+        , backgroundColor theme.colorBackground
+        , color theme.color
+        , textTransform uppercase
+        ]
+
+
+participantStyle : Style
 participantStyle =
     batch
-        [ padding (Css.rem 1) ]
+        [ theme.standardPadding
+        , border3 theme.borderWidth solid theme.color
+        , margin (Css.rem 1)
+        , display inlineBlock
+        , minWidth (Css.em 2)
+        , property "transition" "all 0.3s"
+        ]
 
 
+selectedStyle : Style
 selectedStyle =
     batch
         [ participantStyle
-        , backgroundColor <| hex "ff0000"
-        , color <| hex "ffffff"
+        , backgroundColor theme.color
+        , color theme.colorBackground
+        , transforms [scale 1.3]
         ]
 
 
@@ -126,6 +227,14 @@ renderParticipant participant =
         [ text participant.name ]
 
 
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    if model.isPlaying then
+        Time.every 300 (\time -> Select)
+    else
+        Sub.none
+
+
 
 ---- PROGRAM ----
 
@@ -136,5 +245,5 @@ main =
         { view = view
         , init = init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
